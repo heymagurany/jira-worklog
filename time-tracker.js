40963f2fc1e48b26e0d16cf6f3d2a3e4a7a165b7
Matt Magurany <mmagurany@live.com>
2016-11-23 13:15:36 -0500

    Change JQL

--- time-tracker.js
+++ time-tracker.js
@@ -5,28 +5,26 @@ const request = require('request-promise');
 
 const fromTime = moment(process.argv[2]);
 const toTime = moment(process.argv[3]);
-const workingDuration = moment.duration({
-  days: process.argv[4]
-});
+const workingSeconds = parseInt(process.argv[4]) * 28800;
 
-if (process.argv.length < 5 || !fromTime.isValid() || !toTime.isValid() || isNaN(workingDuration.asDays())) {
+if (process.argv.length < 5 || !fromTime.isValid() || !toTime.isValid() || isNaN(workingSeconds)) {
   console.log("usage: jira2inin <start-date> <end-date> <working-days>");
   return;
 }
 
-const workingSeconds = workingDuration.asDays() * 28800;
 const baseUrl = 'https://inindca.atlassian.net/rest/api/2/';
 const username = 'heymagurany';
 const options = {
   auth: {
     user: username,
-    pass: 'ydYFB4Zp842yF7cJkMFgiZwRA})NVu'
+    pass: ''
   },
   json: true,
   headers: {
     'Accept': 'application/json'
   }
 };
+const epicIssueTypeId = '6';
 const epicFieldName = 'customfield_10300';
 
 function get(path) {
@@ -44,15 +42,15 @@ function post(path, body) {
   }));
 }
 
-function aggregateWorkLogs(worklogs) {
+function aggregateWorkLogs(worklogs, totalTime) {
   return _(worklogs)
         .filter(worklog => worklog.author.name === username)
         .filter(worklog => moment(worklog.started).isBetween(fromTime, toTime))
         .map(worklog => worklog.timeSpentSeconds)
-        .reduce(_.add, 0)
+        .reduce(_.add, totalTime);
 }
 
-function getWorkLog(issues, epicMap, epicKey, whitespace) {
+function getWorkLog(issues, epicMap, epicKey) {
   if (issues) {
     return Promise.each(issues, issue => {
       epicKey = issue.fields[epicFieldName] || epicKey;
@@ -65,23 +63,14 @@ function getWorkLog(issues, epicMap, epicKey, whitespace) {
             console.warn('Worklog does not contain all results.');
           }
 
-          var timeSpent = aggregateWorkLogs(worklog.worklogs);
-          var duration = moment.duration({
-            seconds: timeSpent
-          });
-
-          if (epicMap[epicKey]) {
-            epicMap[epicKey].add(duration);
-          }
-          else {
-            epicMap[epicKey] = duration;
-          }
-
-          // console.log(`${whitespace}${issue.key}: ${epicKey}, ${duration}`);
+          epicMap[epicKey] = aggregateWorkLogs(worklog.worklogs, epicMap[epicKey]);
         }
       }
+      else {
+        console.log(`MISS! ${issue.key}`);
+      }
 
-      return getWorkLog(issue.fields.subtasks, epicMap, epicKey, whitespace + '  ');
+      return getWorkLog(issue.fields.subtasks, epicMap, epicKey);
     });
   }
 }
@@ -89,30 +78,43 @@ function getWorkLog(issues, epicMap, epicKey, whitespace) {
 var epicMap = {};
 
 return post('search', {
-  jql: `project = SUPP AND issuetype in (Bug, Story)`,
+  jql: `worklogAuthor = currentUser() AND worklogDate >= ${fromTime.format('YYYY-MM-DD')} AND worklogDate < ${toTime.format('YYYY-MM-DD')}`,
   startAt: 0,
   maxResults: 1000,
   fields: [
     epicFieldName,
     'subtasks',
-    'worklog'
+    'worklog',
+    'issuetype',
+    'parent'
+  ],
+  expand: [
+    'parent.' + epicFieldName
   ]
 })
 .then(storiesAndBugs => {
+  console.log(storiesAndBugs.issues[0]);
   if (storiesAndBugs.total > storiesAndBugs.maxResults) {
     console.warn('Query result does not contain all results.');
   }
 
-  return getWorkLog(storiesAndBugs.issues, epicMap, null, '');
+  var issuesWithEpic = _.filter(storiesAndBugs.issues, issue => issue.fields[epicFieldName]);
+  var issuesWithoutEpic = _.filter(storiesAndBugs.issues, issue => !issue.fields[epicFieldName]);
+  var epics = _.filter(issuesWithoutEpic, issue => issue.fields.issuetype.id === epicIssueTypeId);
+
+  return getWorkLog(storiesAndBugs.issues, epicMap);
+
+  // TODO: get work log for epics
+  // TODO: get epic for sub-tasks
 })
 .then(() => {
   _.forIn(epicMap, (value, key) => {
-    var secondsLogged = value.asSeconds();
+    var secondsLogged = value;
 
     if (secondsLogged) {
       var percent = (secondsLogged / workingSeconds) * 100;
 
-      console.log(`${key}: ${percent.toFixed(2)}%`);
+      console.log(`${key}: ${secondsLogged} ${percent.toFixed()}%`);
     }
   });
 });
