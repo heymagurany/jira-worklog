b7ce99bb294dce55f3e35a9cce4b73d88cefb4f8
Matt Magurany <mmagurany@live.com>
2017-02-17 13:39:15 -0500

    Move command line input to separate executable script

--- time-tracker.js
+++ time-tracker.js
@@ -5,150 +5,142 @@ const readline = require('readline');
 const request = require('request-promise');
 const Writable = require('stream').Writable;
 
-const fromTime = moment(process.argv[2]);
-const toTime = moment(process.argv[3]);
-const workingSeconds = parseInt(process.argv[4]) * 28800;
-const username = process.argv[5];
-
-if (process.argv.length < 6 || !fromTime.isValid() || !toTime.isValid() || isNaN(workingSeconds)) {
-  console.log("usage: jira2inin <start-date> <end-date> <working-days> <username>");
-  return;
-}
-
-const baseUrl = 'https://inindca.atlassian.net/rest/api/2/';
-const options = {
-  auth: {
-    user: username
-  },
-  json: true,
-  headers: {
-    'Accept': 'application/json'
-  }
-};
-const epicIssueTypeId = '6';
-const epicFieldName = 'customfield_10300';
-
-function auth() {
-  return new Promise((resolve, reject) => {
-    if (options.auth.pass) {
-      resolve(options);
+module.exports = function(fromTime, toTime, workingSeconds, username) {
+  const baseUrl = 'https://inindca.atlassian.net/rest/api/2/';
+  const options = {
+    auth: {
+      user: username
+    },
+    json: true,
+    headers: {
+      'Accept': 'application/json'
     }
+  };
+  const epicIssueTypeId = '6';
+  const epicFieldName = 'customfield_10300';
 
-    var mute = false;
-    var mutedStdout = new Writable({
-      write: function(chunk, encoding, callback) {
-        if (!mute) {
-          process.stdout.write(chunk, encoding);
-        }
-        callback();
+  function auth() {
+    return new Promise((resolve, reject) => {
+      if (options.auth.pass) {
+        resolve(options);
       }
-    });
 
-    var interface = readline.createInterface({
-      input: process.stdin,
-      output: mutedStdout,
-      terminal: true
-    });
-
-    interface.question('password: ', (password) => {
-      mute = false;
-      options.auth.pass = password;
-      interface.write('\n');
-      interface.close();
-      resolve(options);
-    });
-
-    mute = true;
-  });
-}
-
-function get(path) {
-  return auth().then((options) => request(_.assign(options, {
-    method: 'GET',
-    url: baseUrl + path
-  })));
-}
-
-function post(path, body) {
-  return auth().then((options) => request(_.assign(options, {
-    method: 'POST',
-    url: baseUrl + path,
-    body: body
-  })));
-}
-
-function aggregateWorkLogs(worklogs, totalTime) {
-  return _(worklogs)
-        .filter(worklog => worklog.author.name === username)
-        .filter(worklog => moment(worklog.started).isBetween(fromTime, toTime))
-        .map(worklog => worklog.timeSpentSeconds)
-        .reduce(_.add, totalTime);
-}
-
-function getWorkLog(issues, epicMap, epicKey) {
-  if (issues) {
-    return Promise.each(issues, issue => {
-      epicKey = issue.fields[epicFieldName] || epicKey;
-
-      if (epicKey) {
-        var worklog = issue.fields.worklog;
-
-        if (worklog) {
-          if (worklog.total > worklog.maxResults) {
-            console.warn('Worklog does not contain all results.');
+      var mute = false;
+      var mutedStdout = new Writable({
+        write: function(chunk, encoding, callback) {
+          if (!mute) {
+            process.stdout.write(chunk, encoding);
           }
-
-          epicMap[epicKey] = aggregateWorkLogs(worklog.worklogs, epicMap[epicKey]);
+          callback();
         }
-      }
-      else {
-        console.log(`MISS! ${issue.key}`);
-      }
+      });
 
-      return getWorkLog(issue.fields.subtasks, epicMap, epicKey);
+      var interface = readline.createInterface({
+        input: process.stdin,
+        output: mutedStdout,
+        terminal: true
+      });
+
+      interface.question('password: ', (password) => {
+        mute = false;
+        options.auth.pass = password;
+        interface.write('\n');
+        interface.close();
+        resolve(options);
+      });
+
+      mute = true;
     });
   }
-}
 
-var epicMap = {};
-
-return post('search', {
-  jql: `worklogAuthor = currentUser() AND worklogDate >= ${fromTime.format('YYYY-MM-DD')} AND worklogDate < ${toTime.format('YYYY-MM-DD')}`,
-  startAt: 0,
-  maxResults: 1000,
-  fields: [
-    epicFieldName,
-    'subtasks',
-    'worklog',
-    'issuetype',
-    'parent'
-  ],
-  expand: [
-    'parent.' + epicFieldName
-  ]
-})
-.then(storiesAndBugs => {
-  if (storiesAndBugs.total > storiesAndBugs.maxResults) {
-    console.warn('Query result does not contain all results.');
+  function get(path) {
+    return auth().then((options) => request(_.assign(options, {
+      method: 'GET',
+      url: baseUrl + path
+    })));
   }
 
-  var issuesWithEpic = _.filter(storiesAndBugs.issues, issue => issue.fields[epicFieldName]);
-  var issuesWithoutEpic = _.filter(storiesAndBugs.issues, issue => !issue.fields[epicFieldName]);
-  var epics = _.filter(issuesWithoutEpic, issue => issue.fields.issuetype.id === epicIssueTypeId);
+  function post(path, body) {
+    return auth().then((options) => request(_.assign(options, {
+      method: 'POST',
+      url: baseUrl + path,
+      body: body
+    })));
+  }
 
-  return getWorkLog(storiesAndBugs.issues, epicMap);
+  function aggregateWorkLogs(worklogs, totalTime) {
+    return _(worklogs)
+          .filter(worklog => worklog.author.name === username)
+          .filter(worklog => moment(worklog.started).isBetween(fromTime, toTime))
+          .map(worklog => worklog.timeSpentSeconds)
+          .reduce(_.add, totalTime);
+  }
 
-  // TODO: get work log for epics
-  // TODO: get epic for sub-tasks
-})
-.then(() => {
-  _.forIn(epicMap, (value, key) => {
-    var secondsLogged = value;
+  function getWorkLog(issues, epicMap, epicKey) {
+    if (issues) {
+      return Promise.each(issues, issue => {
+        epicKey = issue.fields[epicFieldName] || epicKey;
 
-    if (secondsLogged) {
-      var percent = (secondsLogged / workingSeconds) * 100;
+        if (epicKey) {
+          var worklog = issue.fields.worklog;
 
-      console.log(`${key}: ${secondsLogged} ${percent.toFixed()}%`);
+          if (worklog) {
+            if (worklog.total > worklog.maxResults) {
+              console.warn('Worklog does not contain all results.');
+            }
+
+            epicMap[epicKey] = aggregateWorkLogs(worklog.worklogs, epicMap[epicKey]);
+          }
+        }
+        else {
+          console.log(`MISS! ${issue.key}`);
+        }
+
+        return getWorkLog(issue.fields.subtasks, epicMap, epicKey);
+      });
     }
+  }
+
+  var epicMap = {};
+
+  return post('search', {
+    jql: `worklogAuthor = currentUser() AND worklogDate >= ${fromTime.format('YYYY-MM-DD')} AND worklogDate < ${toTime.format('YYYY-MM-DD')}`,
+    startAt: 0,
+    maxResults: 1000,
+    fields: [
+      epicFieldName,
+      'subtasks',
+      'worklog',
+      'issuetype',
+      'parent'
+    ],
+    expand: [
+      'parent.' + epicFieldName
+    ]
+  })
+  .then(storiesAndBugs => {
+    if (storiesAndBugs.total > storiesAndBugs.maxResults) {
+      console.warn('Query result does not contain all results.');
+    }
+
+    var issuesWithEpic = _.filter(storiesAndBugs.issues, issue => issue.fields[epicFieldName]);
+    var issuesWithoutEpic = _.filter(storiesAndBugs.issues, issue => !issue.fields[epicFieldName]);
+    var epics = _.filter(issuesWithoutEpic, issue => issue.fields.issuetype.id === epicIssueTypeId);
+
+    return getWorkLog(storiesAndBugs.issues, epicMap);
+
+    // TODO: get work log for epics
+    // TODO: get epic for sub-tasks
+  })
+  .then(() => {
+    _.forIn(epicMap, (value, key) => {
+      var secondsLogged = value;
+
+      if (secondsLogged) {
+        var percent = (secondsLogged / workingSeconds) * 100;
+
+        console.log(`${key}: ${secondsLogged} ${percent.toFixed()}%`);
+      }
+    });
   });
-});
+}
